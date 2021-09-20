957fce9a11b1a51013941257f83fb52c179e4082
VStarovoitov <vstarovoitov@it-one.ru>
2021-09-20 20:15:15 +0700

    Добавлена поддержка h2.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO USERS (EMAIL, FIRST_NAME, LAST_NAME, PASSWORD)
+VALUES  ('user@gmail.com', 'User_First', 'User_Last', 'password'),
+        ('vasya@gmail.com', 'User_2', 'User_2', 'pass2'),
+        ('petya@gmail.com', 'User_3', 'User_3', 'pass3'),
+        ('admin@javaops.ru', 'Admin_First', 'Admin_Last', 'admin');
+
+INSERT INTO USER_ROLE (ROLE, USER_ID)
+VALUES ('ROLE_USER', 1),
+       ('ROLE_ADMIN', 2),
+       ('ROLE_USER', 2);
